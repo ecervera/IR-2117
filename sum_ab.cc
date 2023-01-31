bf3bd7aecfe48871550b71bdf693b8086eb652c9
ecervera <ecervera@uji.es>
2023-01-31 12:05:10 +0100

    use do while

--- sum_ab.cc
+++ sum_ab.cc
@@ -4,12 +4,10 @@ int main() {
   int a,b;
   
   int s=0;
-  std::cout << "Enter a: ";
-  std::cin >> a;
-  while (a<1) {
+  do {
     std::cout << "Enter a: ";
     std::cin >> a;
-  }
+  } while (a<1);
   
   std::cout << "Enter b: ";
   std::cin >> b;

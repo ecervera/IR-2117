06960f64e538dfecfe2d2a262b81ab5768c6c3cb
ecervera <ecervera@uji.es>
2023-01-31 12:12:02 +0100

    Update sum_ab.cc

--- sum_ab.cc
+++ sum_ab.cc
@@ -9,8 +9,10 @@ int main() {
     std::cin >> a;
   } while (a<1);
   
-  std::cout << "Enter b: ";
-  std::cin >> b;
+  do {
+    std::cout << "Enter b: "; 
+    std::cin >> b;  
+  } while (b<1);
     
   for(int i=a; i<=b; i++)
     s+=i;

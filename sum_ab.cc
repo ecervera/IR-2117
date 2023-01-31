97770c2607c6e6106080158620558a138c0fb49a
ecervera <ecervera@uji.es>
2023-01-31 11:56:43 +0100

    Update sum_ab.cc

--- sum_ab.cc
+++ sum_ab.cc
@@ -1,3 +1,19 @@
+#include <iostream>
+
 int main() {
+  int a,b;
+  
+  int s=0;
+  std::cout << "Enter a: ";
+  std::cin >> a;
+  std::cout << "Enter b: ";
+  std::cin >> b;
+    
+  for(int i=a; i<=b; i++)
+    s+=i;
+    
+  std::cout << "The sum is " << s << std::endl;
+  
+  return 0;
 }
 
